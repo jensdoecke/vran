008e9ae231ff295751116860b6fbadb31ff1eca2
Gunther <Rethnug>
2016-06-23 20:59:58 +0200

    Wechselkurse app.js und Daten

--- app.js
+++ app.js
@@ -83,4 +83,30 @@ router.get('/laenderinfos', function (req, res) {
 });
 
 
+router.get('/waehrungen', function(req,res) {
+  var daten = require('./misc/Waehrungen.txt');
+  res.send(daten);
+});
+
+router.get('/exchangerates', function(req,res) {
+  var daten = require('./misc/exchangerates.json');
+  var d = new Date();
+  var n = d.getTime();
+  console.log(daten.timestamp)
+  if (daten == null || daten.timestamp - Math.round(n/1000) > 86400) {
+    $https.get('https://openexchangerates.org/api/latest.json?app_id=cc8e8a1f1a014c9faacfbc107e37bbcf').success(function(data) {
+      $scope.daten = data;
+      var fs  = require('fs');
+      fs.unlinkSync('./misc/exchangerates.json');
+      fs.writeFile('./misc/exchangerates.json', data, (err) => {
+	if (err) throw err;
+	console.log('Fehler beim Speichern der Währungskurse!');
+      })
+    });
+  }
+  res.send(daten);
+});
+
+
+
 app.use('/api', router);
